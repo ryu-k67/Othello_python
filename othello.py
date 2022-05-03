7f5ec75d130fc5ef01854ee6316dfb3756ac915a
ryu-k67 <200441067@ccmailg.meijo-u.ac.jp>
2022-05-03 16:16:03 +0900

    端指定時の不具合修正、勝敗の判定と表示

--- othello.py
+++ othello.py
@@ -51,7 +51,7 @@ def search(board,x,y,player):
         if y<8:
             if board[(x-1)*2][(y+1)*2]==sign:
                 flagLoop=False
-                for i in range(2,min(x,8-y)):
+                for i in range(2,min(x,8-y+1)):
                     if board[(x-i)*2][(y+i)*2]==sign:
                         continue
                     elif board[(x-i)*2][(y+i)*2]==correct:
@@ -86,7 +86,7 @@ def search(board,x,y,player):
     if y<8:
         if board[x*2][(y+1)*2]==sign:
             flagLoop=False
-            for i in range(2,8-y):
+            for i in range(2,8-y+1):
                 if board[x*2][(y+i)*2]==sign:
                     continue
                 elif board[x*2][(y+i)*2]==correct:
@@ -105,7 +105,7 @@ def search(board,x,y,player):
         if y>1:
             if board[(x+1)*2][(y-1)*2]==sign:
                 flagLoop=False
-                for i in range(2,min(8-x,y)):
+                for i in range(2,min(8-x+1,y)):
                     if board[(x+i)*2][(y-i)*2]==sign:
                         continue
                     elif board[(x+i)*2][(y-i)*2]==correct:
@@ -121,7 +121,7 @@ def search(board,x,y,player):
 
         if board[(x+1)*2][y*2]==sign:
             flagLoop=False
-            for i in range(2,8-x):
+            for i in range(2,8-x+1):
                 if board[(x+i)*2][y*2]==sign:
                     continue
                 elif board[(x+i)*2][y*2]==correct:
@@ -138,7 +138,7 @@ def search(board,x,y,player):
         if y<8:
             if board[(x+1)*2][(y+1)*2]==sign:
                 flagLoop=False
-                for i in range(2,min(8-x,8-y)):
+                for i in range(2,min(8-x+1,8-y+1)):
                     if board[(x+i)*2][(y+i)*2]==sign:
                         continue
                     elif board[(x+i)*2][(y+i)*2]==correct:
@@ -171,8 +171,10 @@ def main():
     printBoard(board)
 
     player=0
-    while True:
-        print('プレイヤー',player+1,'：座標を入力してください')
+    color=['黒','白']
+    loop=1
+    while loop<=60:
+        print('プレイヤー',player+1,'(',color[player],')：座標を入力してください')
         print('縦　横 :',end='')
         x,y=map(int,input().split())
 
@@ -180,10 +182,28 @@ def main():
             if search(board,x,y,player):
                 printBoard(board)
                 player=(player+1)%2
+                loop+=1
             else:
                 print('そこには置けません')
         else:
             print('指定された座標は範囲外です')
+
+    print('--------ゲーム終了！--------')
+    black=0
+    white=0
+    for i in range(1,9):
+        for j in range(1,9):
+            if board[i*2][j*2]=='○':
+                black+=1
+            elif board[i*2][j*2]=='●':
+                white+=1
+    print('プレイヤー1：',black,'枚\nプレイヤー2；',white,'枚')
+    if black>white:
+        print('プレイヤー1の勝利！')
+    elif black<white:
+        print('プレイヤー2の勝利！')
+    else:
+        print('引き分け！')
 
 
 if __name__=="__main__":
